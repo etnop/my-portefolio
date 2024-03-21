488b7d0f6ea338fdeed760756fc37fafbc8afaf6
Pedro Ponte <pedroponte@sisdias.com>
2024-03-21 15:29:30 -0100

    feature: configs material ui theme

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#232323",
+    },
+    secondary: {
+      main: "#4f8e3e",
+    },
+  },
+  typography: {
+    fontFamily:[ "Helvetica Neuw"].join(',')
+  }
+});
+theme= responsiveFontSizes(theme)
+export default theme;
