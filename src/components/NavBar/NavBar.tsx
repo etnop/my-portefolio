fe6a82838044ba7e6120117e34e8a2f89b4eb757
Pedro Ponte <pedroponte@sisdias.com>
2024-03-21 15:35:34 -0100

    feat: init Navbar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material";
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-evenly",
+}));
+
+const NavBar = () => {
+  return (
+    <>
+      <AppBar position="absolute">
+        <StyledToolbar>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Skills</MenuItem>
+          <MenuItem>Projects</MenuItem>
+        </StyledToolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default NavBar;
